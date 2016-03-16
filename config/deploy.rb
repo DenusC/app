7c9dc31411986f4180eecc75dca33a79126c05c9
cyy <1018153281@qq.com>
2016-03-16 14:53:25 +0800

    cap deploy success without server on

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'my_app'
+set :repo_url, 'git@github.com:DenusC/app.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
+  set :deploy_to, "/home/deus/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,8 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server '192.168.3.23', user: 'deus', roles:%w(db)
+set :branch, 'master'
 
 
 # role-based syntax
