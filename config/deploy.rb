f0a2866dd699d3672e84a494187f424642deab97
cyy <1018153281@qq.com>
2016-03-18 14:13:07 +0800

    remove datebase.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :repo_url, 'git@github.com:DenusC/app.git'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
- set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/shared')
+ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/shared',
+                                               'config/database.yml')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
