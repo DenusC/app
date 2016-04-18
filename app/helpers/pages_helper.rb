ba030278249e13f162d8334f6e4fefcf0bd4414a
cyy <1018153281@qq.com>
2016-04-18 19:09:45 +0800

    add viewport

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  def menu(link, title = nil, &blk)
+    if block_given?
+      content_tag(:li, class: 'ui-border-r' , data:{ href: link }, &blk)
+    else
+      content_tag(:li,
+        content_tag(:a, title, href: link),
+        class: 'ui-border-r' , data:{ href: link }
+      )
+    end
+  end
 end
